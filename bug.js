eb26d5d0a8ffb39ed63916036d213a8d175f3756
BugHunter-X <askzenai@gmail.com>
2025-01-20 21:28:09 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,30 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href='/about'>
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error if the dynamic route is not handled properly
+  const { query } = useRouter();
+  console.log(query); // Accessing query parameters
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+```
